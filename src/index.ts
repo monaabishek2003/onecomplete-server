51fcb47c5ef8cefd53d5a00d3ef6d0e2e4032b38
monaabishek2003 <107188270+monaabishek2003@users.noreply.github.com>
2025-01-12 23:45:13 +0530

    Feature/auth (#4)
    * clerkClient configured corresponding a routes and controller initialised
    
    * updated gitignore
    
    * updated routes config in index

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import { createClerkClient, clerkMiddleware } from "@clerk/express";
 
 // ROUTE IMPORTS
 import courseRoutes from "./routes/courseRoutes"
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 // CONFIGURATION
 dotenv.config();
@@ -15,6 +17,10 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
 
 if(!isProduction) dynamoose.aws.ddb.local();
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY
+})
 
 const app = express();
 app.use(express.json());
@@ -24,13 +30,14 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use(cors());
+app.use(clerkMiddleware())
 
 // ROUTES
-
 app.get("/",(req,res)=>{
   res.send("Hello World")
 });
 app.use("/courses",courseRoutes)
+app.use("users/clerk", userClerkRoutes)
 
 const port = process.env.port || 5000;
 
